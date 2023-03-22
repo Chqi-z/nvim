b0cde0186f07a9144daaa52e366c960232923335
cc <cc@cc.com>
2023-03-22 16:21:45 +0800

    init

---
+++ lua/config/init.lua
@@ -0,0 +1,62 @@
+---@type LazyVimConfig
+local M = {}
+
+---@class LazyVimConfig
+-- icons used by other plugins
+M.icons = {
+	diagnostics = {
+		Error = " ",
+		Warn = " ",
+		Hint = " ",
+		Info = " ",
+	},
+	git = {
+		added = " ",
+		modified = " ",
+		removed = " ",
+	},
+	kinds = {
+		Array = " ",
+		Boolean = " ",
+		Class = " ",
+		Color = " ",
+		Constant = " ",
+		Constructor = " ",
+		Copilot = " ",
+		Enum = " ",
+		EnumMember = " ",
+		Event = " ",
+		Field = " ",
+		File = " ",
+		Folder = " ",
+		Function = " ",
+		Interface = " ",
+		Key = " ",
+		Keyword = " ",
+		Method = " ",
+		Module = " ",
+		Namespace = " ",
+		Null = "ﳠ ",
+		Number = " ",
+		Object = " ",
+		Operator = " ",
+		Package = " ",
+		Property = " ",
+		Reference = " ",
+		Snippet = " ",
+		String = " ",
+		Struct = " ",
+		Text = " ",
+		TypeParameter = " ",
+		Unit = " ",
+		Value = " ",
+		Variable = " ",
+	},
+}
+
+require("config.options")
+require("config.lazy")
+require("config.keymaps")
+require("config.autocmds")
+
+return M
